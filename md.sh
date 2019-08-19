640f221d3ca91bcb26d5076536036a20e644f20d
marc-antoine <marcantoine.bruelhart@gmail.com>
2019-08-19 19:27:49 +0200

    create simple md framework

---
+++ md.sh
@@ -0,0 +1,35 @@
+
+## https://misc.flogisoft.com/bash/tip_colors_and_formatting
+
+# Define the colors
+TITLE="\e[1m"
+TITLE_CLEAN="\e[0m"
+SUBTITLE="\e[34m"
+#SUBTITLE="\e[1m"
+COMMAND="\e[94m"
+#COMMAND="\e[32m"
+CLEAN="\e[0m"
+
+## BLUE 34mBlue
+## LIGHT BLUE 94mLight blue"
+
+## this function must be implemented into the mab framework
+function helpme () {
+    echo -e ""
+    echo -e "---------------------------------------------------------------------"
+    echo -e "${TITLE}Mabs help to use the full potential of the md framework ${TITLE_CLEAN}"
+    echo -e "---------------------------------------------------------------------"
+    echo -e ""
+    echo -e "${SUBTITLE}Usage:${CLEAN}  ${COMMAND}md <Command>${CLEAN}"
+    echo -e ""
+    echo -e "${SUBTITLE}Commands for development environment${CLEAN}"
+    echo -e ""
+    echo -e "${SUBTITLE}General commands${CLEAN}"
+    echo -e ""
+    echo -e "        ${COMMAND}about${CLEAN}         Show the general information"
+    echo -e "        ${COMMAND}help${CLEAN}          Show this help"
+    echo -e "        ${COMMAND}version${CLEAN}       Show the version"
+    echo -e ""
+}
+
+helpme

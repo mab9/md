33ef4101f19665d9a5e60ee25ac442a04f2e4ec4
marc-antoine <marcantoine.bruelhart@gmail.com>
2019-09-17 15:36:21 +0200

    write the mvn settings change script

--- md.sh
+++ md.sh
@@ -34,8 +34,58 @@ function helpme () {
     echo -e ""
 }
 
+
+##
+# Ideas
+
+# add plugins, everyone can create theyr own plugin in bash or python or java?
+# load plugins that are in a folder and list them
+
+# create scripts for repeated tasks
+# easy share of those scripts
+# easy installation of plugins in different languages
+# description of those plugins and how to install the framework
+
+###############
+
+
+
+
+
+## use the following mvn settings
+# . make shure the maven settings path is set
+# . make shure the maven setting that will be used is found in the /developement/config folder
+# . change the maven setting
+#
+## change default maven setting
+# . list all mvn settings
+# . choose one of them
+# . set as the default
+
+
+function mvn_settings_change() {
+  settings=( $(ls ~/.m2/ | grep -i settings) )
+  settings+=(exit)
+  echo yey ${#settings[@]}
+
+  select setting in "${settings[@]}"; do
+      [[ $setting == exit ]] && break
+
+      ## break if value is between the size of the array
+      if [[ "$setting" < "${#settings[@]}" ]] ; then
+        echo You have to chose a number between 1 and ${#settings[@]}
+      else
+        # actualize the mvn setting
+        echo "You have chosen: $setting"
+        exit
+      fi
+    done
+}
+
+
 function version() {
   echo "MD version ${VERSION}"
 }
 
 helpme
+selectTest

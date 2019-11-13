8c8dedc1023e751aa964babc01107226d883ba1a
marcantoine <marcantoine.bruelhart@gmail.com>
2019-11-13 09:33:03 +0100

    extend cmd ansalye to print 3 args

--- core/cmdanalyse/cmdanalyse.sh
+++ core/cmdanalyse/cmdanalyse.sh
@@ -2,5 +2,5 @@
 
 HISTFILE=~/.bash_history
 set -o history
-history | awk 'BEGIN {FS="[ \t]+|\\|"} {print $3}' | sort | uniq -c | sort -nr | head -n 20
+history | awk 'BEGIN {FS="[ \t]+|\\|"} {print $3, $4, $5}' | sort | uniq -c | sort -nr | head -n 20
 

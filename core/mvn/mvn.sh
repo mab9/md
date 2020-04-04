fa649d77c63c9b864190cd7a197195040abcc329
marcantoine <marc-antoine.bruelhart@bedag.ch>
2020-04-04 20:54:06 +0200

    Refactor mvn change setting

--- core/mvn/mvn.sh
+++ core/mvn/mvn.sh
@@ -1,12 +1,13 @@
 ######
-# Change the default mvn setting, so that it can be used without
-# explicit declaration.
 #
-# List all mvn settings located at the default place,
-# let the user choose a config file and replace the default setting.
-#
+# Change default mvn settings with prepared settings.
+# Locate mvn settings with the key word settings and label at the default location  ~/.m2.
+# All files containing settings key word will be configurable as default setting. Therefore
+# the default file settings.xml is replaced with the choosen one.
 
-function execute() {
+function changeSetting() {
+  echo "Select new default mvn setting"
+  echo ""
   settings=( $(ls ~/.m2/ | grep -i settings) )
   settings+=(exit)
 
@@ -25,4 +26,52 @@ function execute() {
     done
 }
 
-execute
+function listSettings() {
+  echo "List all mvn settings located at the default path ~/.m2"
+  echo ""
+  ls ~/.m2 | grep -i settings
+}
+
+function instructions() {
+  echo "Options:"
+  echo "  -c | --change-setting:      Change mvn setting"
+  echo "  -l | --list-settings:       List mvn settings"
+  echo "       --help:                Show help"
+  echo ""
+  echo "Usage:"
+  echo "  md mvn [Options]"
+  echo ""
+}
+
+function error() {
+  echo "Examples: md mvn -c"
+  echo ""
+  instructions
+}
+
+function help() {
+  echo "Help"
+  echo ""
+  instructions
+}
+
+function executeDefaults() {
+  command="$2" # second layer
+
+  case $command in
+      --change-setting|-c)
+          changeSetting
+          ;;
+      --list-settings|-l)
+          listSettings
+          ;;
+      --help)
+          help
+          ;;
+      *)
+          error
+          ;;
+  esac
+}
+
+executeDefaults $@

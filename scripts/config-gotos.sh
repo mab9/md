fa5e69c0353fdf155b7cca76bde2889166f40ae9
marcantoine <marc-antoine.bruelhart@bedag.ch>
2020-08-27 06:06:38 +0200

    Add new goto entry

--- scripts/config-gotos.sh
+++ scripts/config-gotos.sh
@@ -4,6 +4,7 @@ mdConfigFile=${HOME}/.md
 sourceDir=$(cat $mdConfigFile | grep -i source-dir | cut -d "=" -f 2)
 
 alias goto-svv-src="cd ${sourceDir}/svvstatistikserver"
+alias goto-svv-src-dep="cd ${sourceDir}/svvstatistikserver/statistikserver-deployment/docker/helm/statistikserver"
 alias goto-dab-src="cd ${sourceDir}/dabbawala"
 alias goto-md-src="cd ${MD_INSTALLATION_FOLDER}"
 alias goto-cs-src="cd ${sourceDir}/cheatsheets"

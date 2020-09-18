b95a9f66d1d1ef684726f142e89b774452fc588f
marc-antoine <marcantoine.bruelhart@gmail.com>
2020-09-18 11:41:33 +0200

    Add new pentaho aliases

--- scripts/config-frameworks.sh
+++ scripts/config-frameworks.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+
+alias pentaho.metadata-editor='bash /home/mab/development/apps/pentaho/metadata-editor/metadata-editor.sh'
+alias pentaho.spoon='pentaho-data-integration-spicule.spoon'
+alias pentaho.report-designer='bash /home/mab/development/apps/pentaho/report-designer/report-designer.sh'
+
+function kwatch() { 
+    watch -n 1 kubectl get pods
+}
+
+
+
+
 # kubectl autocompletion
 source <(kubectl completion bash)
 

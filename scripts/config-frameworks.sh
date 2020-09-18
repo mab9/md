89d057ce860e26f959565c3f7977ca75664c8a5a
marc-antoine <marcantoine.bruelhart@gmail.com>
2020-09-18 11:42:16 +0200

    Remove changes that were commited by accident

--- scripts/config-frameworks.sh
+++ scripts/config-frameworks.sh
@@ -1,16 +1,6 @@
 #!/bin/bash
 
 
-alias pentaho.metadata-editor='bash /home/mab/development/apps/pentaho/metadata-editor/metadata-editor.sh'
-alias pentaho.spoon='pentaho-data-integration-spicule.spoon'
-alias pentaho.report-designer='bash /home/mab/development/apps/pentaho/report-designer/report-designer.sh'
-
-function kwatch() { 
-    watch -n 1 kubectl get pods
-}
-
-
-
 
 # kubectl autocompletion
 source <(kubectl completion bash)

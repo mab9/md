fae5c4b4a73d13094c45e87d42bf8c789afb7f4b
marc-antoine <marcantoine.bruelhart@gmail.com>
2021-02-13 10:34:45 +0100

    Remove unused aliases
    Stash work

---
+++ scripts/config-functions.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# todo replace those functions into dediacted plugins!
+
+
+mdConfigFile=${HOME}/.md
+workingProject=$(cat $mdConfigFile | grep -i working-project | cut -d "=" -f 2)
+sourceDir=$(cat $mdConfigFile | grep -i source-dir | cut -d "=" -f 2)
+
+# start lunatic mail server
+alias start-lunatic="java -jar ${HOME}/development/apps/LunaticSMTP-v0.3.0.jar &" # todo add it into startup script
+alias weather="curl wttr.in/Bern"
+alias start-mailhog="sudo docker run -d -p 1025:1025 -p 8025:8025 mailhog/mailhog"
+
+# uses the default settings.xml ... the one chosen with mvn_change_setting
+alias mcis="mvn clean install -DskipTests"
+
+# change the Java version
+alias change-javav="sudo update-alternatives --config java"
+
+alias c="clear"
+#docker images | grep -i $1 | awk '{print $3}' | xargs docker rmi;"
+
+# https://www.maketecheasier.com/run-bash-commands-background-linux/ open in the background
+alias pentaho.spoon='nohup pentaho-data-integration-spicule.spoon &>/dev/null & && exit'
+alias pentaho.report-designer='nohup bash /home/mab/development/apps/pentaho/report-designer/report-designer.sh &>/dev/null &'
+alias pentaho.metadata-editor='nohup bash /home/mab/development/apps/pentaho/metadata-editor/metadata-editor.sh &>/dev/null &'
+
+
+
+

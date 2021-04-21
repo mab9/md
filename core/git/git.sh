2df8bc4e38471696d23efd10500f7fc928b2411a
marc-antoine <marcantoine.bruelhart@gmail.com>
2021-04-21 13:54:20 +0200

    add some fancy new stuff

---
+++ core/git/git.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+
+echo "new core functions coming soon!"
+
+
+echo "create tag"
+echo "git tag 1.0.0"
+
+echo ""
+echo "push tags"
+echo "git push origin --tags"
+
+echo ""
+echo "delete tag "
+echo "git tag -d 1.0.0"
+echo "Note that this does not remove the tag from any remote servers."
+
+echo ""
+echo "delete tag from any remote servers"
+echo "git push origin --delete 1.0.0"
+# $ git tag -d v1.4-lw
+# Deleted tag 'v1.4-lw' (was e7d5add)
+#
+# Note that this does not remove the tag from any remote servers. There are two common variations for deleting a tag from a remote server.
+#
+# The first variation is git push <remote> :refs/tags/<tagname>:
+#
+# $ git push origin :refs/tags/v1.4-lw
+# To /git@github.com:schacon/simplegit.git
+#  - [deleted]         v1.4-lw
+#
+# The way to interpret the above is to read it as the null value before the colon is being pushed to the remote tag name, effectively deleting it.
+#
+# The second (and more intuitive) way to delete a remote tag is with:
+#
+# $ git push origin --delete <tagname>
+

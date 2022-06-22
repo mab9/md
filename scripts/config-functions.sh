7ec8d415e2ce35b66720481a918508a9ebb609d2
marc-antoine bruelhart <marc-antoine.bruelhart@bedag.ch>
2022-06-22 21:36:35 +0200

    Update aliases

--- scripts/config-functions.sh
+++ scripts/config-functions.sh
@@ -22,9 +22,8 @@ alias c="clear"
 #docker images | grep -i $1 | awk '{print $3}' | xargs docker rmi;"
 
 # https://www.maketecheasier.com/run-bash-commands-background-linux/ open in the background
-alias pentaho.spoon-8='nohup pentaho-data-integration-spicule.spoon &>/dev/null &'
 alias pentaho.spoon-9='nohup /opt/pentaho/pdi-9.3.0/spoon.sh &>/dev/null &'
-alias pentaho.report-designer='nohup bash /home/mab/development/apps/pentaho/report-designer/report-designer.sh &>/dev/null &'
+alias pentaho.report-designer='nohup bash /opt/pentaho/prp-7.1/report-designer.sh &>/dev/null &'
 alias pentaho.metadata-editor='nohup bash /home/mab/development/apps/pentaho/metadata-editor/metadata-editor.sh &>/dev/null &'
 alias pentaho.server-7.1.0.0-12='nohup bash /home/mab/development/apps/pentaho/pentaho-server-7.1.0.0-12/start-pentaho.sh &>/dev/null &'
 alias pentaho.server-8.2.0.0-342='nohup bash /home/mab/development/apps/pentaho/pentaho-server-8.2.0.0-342/start-pentaho.sh &>/dev/null &'
